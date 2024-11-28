5fcf86b750755360eddb113fbfd299cc8d4247f1
HasithaDilshan <hasithadilshandissanayaka@gmail.com>
2024-11-28 19:49:31 +0530

    c1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
-import { ConfigurationComponent } from './components/configuration/configuration.component';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LogTerminalComponent } from './components/log-terminal/log-terminal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    ConfigurationComponent,
-    DashboardComponent,
-    LogTerminalComponent
   ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  title: string = "tickets-fe";
 
 }
